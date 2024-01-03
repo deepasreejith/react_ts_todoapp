6ecbff330f78c62d199f63885f4064c3abc6bb42
deepa <deepakh2014@gmail.com>
2024-01-03 14:44:26 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import ImageHeader from './components/ImageHeader'
+
+function App() {
+ 
+
+  return (
+   <div className='h-[100vh] flex flex-col justify-center items-center'>
+    <ImageHeader />
+    </div>
+  )
+}
+
+export default App
